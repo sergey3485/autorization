9b460d90e36d803adb4679203dc87da9983d56b9
sergey <33asasergey33@gmail.com>
2023-04-19 22:03:34 +0300

    add storybook, project architecture

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,12 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from 'next/app';
+import { EffableProvider } from '@effable/react';
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
+const App = ({ Component, pageProps }: AppProps) => {
+  return (
+    <EffableProvider>
+      <Component {...pageProps} />
+    </EffableProvider>
+  );
+};
+
+export default App;

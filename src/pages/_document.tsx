7b7ba16bdf222c8ff65db2336581df56bad61cb0
sergey <33asasergey33@gmail.com>
2023-04-25 21:17:08 +0300

    feat: add InitializeColorMode

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,12 +1,14 @@
 import {
   Html, Head, Main, NextScript,
 } from 'next/document';
+import { InitializeColorMode } from '@effable/react';
 
 const Document = () => {
   return (
     <Html lang="en">
       <Head />
       <body>
+        <InitializeColorMode />
         <Main />
         <NextScript />
       </body>
